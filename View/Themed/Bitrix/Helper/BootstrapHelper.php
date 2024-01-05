e56b4807c386364902d930a8332ef2886303e56b
avaniashev <avaniashev@gmail.com>
2024-01-05 17:15:13 +0300

    Marketplace

--- View/Themed/Bitrix/Helper/BootstrapHelper.php
+++ View/Themed/Bitrix/Helper/BootstrapHelper.php
@@ -5,11 +5,12 @@ class BootstrapHelper extends AppHelper
     public $helpers = ['Html', 'Form'];
 
     public function floatingInput($field, $options){
-        $defaults = ['label' => ''];
+        $defaults = ['label' => '', 'type' => 'text'];
         $defaults = array_merge($defaults, $options);
         return $this->Html->div('form-floating mb-3',
             $this->Form->input($field, [
-                'label' => false, 'div' => false, 'placeholder' => $defaults['label'], 'class' => 'form-control']).
+                'label' => false, 'div' => false, 'placeholder' => $defaults['label'], 'class' => 'form-control',
+                'type' => $defaults['type']]).
             $this->Form->label($field, $defaults['label']));
     }
 }
